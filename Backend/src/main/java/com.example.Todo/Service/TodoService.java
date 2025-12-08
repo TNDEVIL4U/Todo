43f80c409eb95207824ad59d133d26e71268537b
T S BHUVANESHWAR <bhuvaneshwarts1@gmail.com>
2025-12-08 09:04:45 +0530

    Update TodoService.java

--- Backend/src/main/java/com.example.Todo/Service/TodoService.java
+++ Backend/src/main/java/com.example.Todo/Service/TodoService.java
@@ -1,1 +1,33 @@
+package com.example.Todo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+    public Todo createTodo(Todo todo)
+    {
+        return todoRepository.save(todo);
+    }
+    public Todo getTodoById(Long id) {return todoRepository.findById(id).orElseThrow(()-> new RuntimeException("Todo not found"));}
+    public void deleteTodoById(Long id)
+    {
+        todoRepository.deleteById(id);
+    }
+    public List<Todo> retrieveAllTodos()
+    {
+        return todoRepository.findAll();
+    }
+    public Todo updateTodo(Todo todo)
+    {
+        return todoRepository.save(todo);
+    }
+    public void deleteTodo()
+    {
+        todoRepository.deleteAll();
+    }
+}
